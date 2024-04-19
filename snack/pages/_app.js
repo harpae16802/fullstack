b88a77d9f2413f8b0be4e9e279684f3cec4d40c1
YUNRU <lu913526@gmail.com>
2024-04-19 17:09:07 +0800

    測試衝突

--- snack/pages/_app.js
+++ snack/pages/_app.js
@@ -30,12 +30,8 @@ function MyApp({ Component, pageProps }) {
     <CustomContextProvider>
     <SellerProvider>
       <AuthChecker>
-<<<<<<< HEAD
-      {getLayout(<Component {...pageProps} />)}
-=======
 
        {getLayout( <Component {...pageProps} />)}
->>>>>>> origin/main
         <script
           src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
           integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
