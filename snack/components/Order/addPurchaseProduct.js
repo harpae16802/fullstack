802e06f3888a2a6d218036369167376e8b9adabe
Harpae <harpae16802@gmail.com>
2024-05-03 12:04:10 +0800

    line

--- snack/components/Order/addPurchaseProduct.js
+++ snack/components/Order/addPurchaseProduct.js
@@ -9,6 +9,7 @@ import {
 } from '../../pages/seller-basic-data/config'
 import styles from '@/styles/Order.module.css'
 import { useAuth } from '@/contexts/custom-context'
+import { Modal, Button } from 'react-bootstrap'
 
 const DiscountContentItem = ({ items = [] }) => {
   // 拿取custom_id
@@ -26,6 +27,11 @@ const DiscountContentItem = ({ items = [] }) => {
 
   // 從後端接收到的折扣信息，假定默認使用第一個折扣
   const [selectedDiscount, setSelectedDiscount] = useState(null)
+
+  // 使用彈出視窗
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = () => setShowModal(true)
+  const handleCloseModal = () => setShowModal(false)
 
   //  從購物車來的數據 (使用者想購買的商品 )
   useEffect(() => {
@@ -70,8 +76,8 @@ const DiscountContentItem = ({ items = [] }) => {
 
   // 結帳付款(發送請球)
   const handleLinePay = async () => {
-    const currentTime = new Date().toISOString().replace(/\.\d+Z$/, '') + 'Z'; // 精簡時間格式
-    
+    const currentTime = new Date().toISOString().replace(/\.\d+Z$/, '') + 'Z' // 精簡時間格式
+
     const linePayRequest = {
       amount: Math.round(finalAmount),
       currency: 'TWD',
@@ -81,44 +87,55 @@ const DiscountContentItem = ({ items = [] }) => {
           id: `package${currentTime}`,
           amount: Math.round(finalAmount),
           products: [
-            { name: "夜市獵人訂單", quantity: 1, price: Math.round(finalAmount) }
-          ]
-        }
+            {
+              name: '夜市獵人訂單',
+              quantity: 1,
+              price: Math.round(finalAmount),
+            },
+          ],
+        },
       ],
       redirectUrls: {
-        confirmUrl: "http://127.0.0.1:3000/order/orderStep3",
-        cancelUrl: "http://127.0.0.1:3000/order/orderStep3"
-      }
-    };
-  
+        confirmUrl: 'http://127.0.0.1:3000/order/orderStep3',
+        cancelUrl: 'http://127.0.0.1:3000/order/orderStep3',
+      },
+    }
+
     try {
-      const response = await axios.post('http://127.0.0.1:3002/backRoute/linePayBox',linePayRequest, {
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await axios.post(
+        'http://127.0.0.1:3002/backRoute/linePayBox',
+        linePayRequest,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         }
-      });
-      console.log('LINE Pay 響應:', response.data);
+      )
+      console.log('LINE Pay 響應:', response.data)
       handleLinePayResponse(response)
       // 這裡可以將頁面導向到 LINE Pay 的支付頁面或處理其他邏輯
     } catch (error) {
-      console.error('LINE Pay 請求錯誤', error);
+      console.error('LINE Pay 請求錯誤', error)
     }
   }
 
   //處裡付款(處裡回傳資料)
   const handleLinePayResponse = (response) => {
-    const { returnCode, returnMessage, info: { paymentUrl, transactionId } } = response.data;
+    const {
+      returnCode,
+      returnMessage,
+      info: { paymentUrl, transactionId },
+    } = response.data
 
-    if (returnCode === "0000") {
-      console.log("Success:", returnMessage);
-      console.log("Transaction ID:", transactionId);
-      const urlToOpen = paymentUrl.web; 
-      window.open(urlToOpen, '_blank');
+    if (returnCode === '0000') {
+      console.log('Success:', returnMessage)
+      console.log('Transaction ID:', transactionId)
+      const urlToOpen = paymentUrl.web
+      window.open(urlToOpen, '_blank')
     } else {
-      console.error("Error:", returnMessage);
+      console.error('Error:', returnMessage)
     }
-  };
-
+  }
 
   // 計算訂單的總金額
   const totalAmount = items.reduce((acc, item) => acc + item.total_price, 0)
@@ -338,7 +355,7 @@ const DiscountContentItem = ({ items = [] }) => {
               className={styles.methodImage}
             />
 
-            <p className={styles.paymentText} onClick={handleLinePay}>
+            <p className={styles.paymentText} onClick={handleShowModal}>
               LINE繳費
             </p>
           </div>
@@ -368,7 +385,27 @@ const DiscountContentItem = ({ items = [] }) => {
           </div>
         </div>
       </div>
+      <Modal show={showModal} onHide={handleCloseModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>是否使用LINE Pay 支付?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>即將跳轉頁面 ... </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" className={styles.secondary} onClick={handleCloseModal}>
+            取消支付
+          </Button>
+          <Button variant="primary" className={styles.btnPrimary} onClick={() => {
+            handleCloseModal(); // 先關閉 Modal
+            handleLinePay(); // 然後執行支付
+          }}>
+            確認支付
+          </Button>
+        </Modal.Footer>
+      </Modal>  
     </div>
+    
   )
 }
 
