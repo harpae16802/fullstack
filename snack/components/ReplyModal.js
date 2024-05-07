656045ddcf26ea98c99bb93c5aa54a385b15026c
YUNRU <lu913526@gmail.com>
2024-05-07 19:05:12 +0800

    merge

--- snack/components/ReplyModal.js
+++ snack/components/ReplyModal.js
@@ -1,57 +1,62 @@
-import React, { useState } from 'react'
-import styles from '../styles/navbar-seller.module.scss'
-import { Modal, Button, Form } from 'react-bootstrap'
+import React, { useState, useCallback } from 'react';
+import { Modal, Button, Form, Alert } from 'react-bootstrap';
+import styles from '../styles/navbar-seller.module.scss';
 
+function ReplyModal({ show, onHide, commentId, submitReply, commentContent }) {
+  const [reply, setReply] = useState('');
+  const [showAlert, setShowAlert] = useState(false);  
 
-function ReplyModal({ show, onHide, commentId, submitReply ,commentContent}) {
-  const [reply, setReply] = useState('')
-
-  const handleSubmit = () => {
+  const handleSubmit = useCallback(() => {
     if (reply.trim() === '') {
-      // 如果回复内容为空，则不执行提交回复的操作
+      setShowAlert(true);  
       return;
     }
     submitReply(commentId, reply);
     setReply('');
     onHide();
-  }
+  }, [commentId, reply, submitReply, onHide]);
 
   return (
-    <Modal show={show} onHide={onHide} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>回覆評論</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div>
-          <p>
-            <strong>評論內容：</strong>
-          </p>
-          <p>{commentContent}</p>
-        </div>
-        <Form>
-          <Form.Group className="mb-3">
-            <Form.Label>回覆內容</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              maxLength={1000} 
-              value={reply}
-              onChange={(e) => setReply(e.target.value)}
-              placeholder="輸入回復(最大限制1000字)..."
-            />
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary"   onClick={onHide}>
-          取消
-        </Button>
-        <Button variant="primary"  className={styles.btnPrimary} onClick={handleSubmit}>
-          回覆評論
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  )
+    <>
+      <Modal show={show} onHide={onHide} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>回覆評論</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {showAlert && (
+            <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+              您還沒有輸入內容唷
+            </Alert>
+          )}
+          <div>
+            <p><strong>評論內容:</strong></p>
+            <p>{commentContent}</p>
+          </div>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>回覆内容</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                maxLength={1000}
+                value={reply}
+                onChange={(e) => setReply(e.target.value)}
+                placeholder="输入回覆(最大限制1000字)..."
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onHide}>
+            取消
+          </Button>
+          <Button variant="primary" className={styles.btnPrimary} onClick={handleSubmit}>
+            回覆評論
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
-export default ReplyModal
+export default ReplyModal;
